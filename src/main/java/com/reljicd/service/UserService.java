33bc84705b9897a7d7ef5e7f0c34ae31412cf18d
Dušan Reljić <reljicd@gmail.com>
2018-03-23 13:32:42 +0100

    refactoring to use Optional, and cleanup

--- src/main/java/com/reljicd/service/UserService.java
+++ src/main/java/com/reljicd/service/UserService.java
@@ -2,14 +2,13 @@ package com.reljicd.service;
 
 import com.reljicd.model.User;
 
-/**
- * Created by Dusan on 19-May-17.
- */
+import java.util.Optional;
+
 public interface UserService {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     User saveUser(User user);
 
