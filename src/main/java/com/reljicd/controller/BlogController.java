6b1cee0684e97c2dd28773a58f8ed5171838a822
Dušan Reljić <reljicd@gmail.com>
2018-03-29 12:44:15 +0200

    removed unused import

--- src/main/java/com/reljicd/controller/BlogController.java
+++ src/main/java/com/reljicd/controller/BlogController.java
@@ -7,9 +7,11 @@ import com.reljicd.service.UserService;
 import com.reljicd.util.Pager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
